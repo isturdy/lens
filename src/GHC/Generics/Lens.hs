245fd51f955406bfbd2c7d21b9921b72ffb6f34b
isturdy <sturdyi12@mail.wlu.edu>
2013-08-07 16:20:17 -0400

    Updated GHC.Generics.Lens documentation.
    Control.Lens.Representable and its associated name conflict vanished in
    3.8.

--- src/GHC/Generics/Lens.hs
+++ src/GHC/Generics/Lens.hs
@@ -16,12 +16,10 @@
 -- You can use hiding or imports to mitigate this to an extent, and the following imports,
 -- represent a fair compromise for user code:
 --
--- > import Control.Lens hiding (Rep)
+-- > import Control.Lens
 -- > import GHC.Generics hiding (from, to)
 --
--- You can use 'generic' to replace 'GHC.Generics.from' and 'GHC.Generics.to' from @GHC.Generics@,
--- and probably won't be explicitly referencing 'Control.Lens.Representable.Rep' from @Control.Lens@
--- in code that uses generics.
+-- You can use 'generic' to replace 'GHC.Generics.from' and 'GHC.Generics.to' from @GHC.Generics@.
 --
 -- This module provides compatibility with older GHC versions by using the
 -- <http://hackage.haskell.org/package/generic-deriving generic-deriving>
